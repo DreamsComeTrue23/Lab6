95edd43f2a605633aefe27ebd5e589ae92adba05
Özge Bostan <152620328+DreamsComeTrue23@users.noreply.github.com>
2025-03-27 15:14:40 +0300

    Create lab6.cpp

---
+++ lab6.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+double h_function(int n)
+{
+    if (n == 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return 1.0 / n + h_function(n - 1);
+    }
+}
+
+double h_function()
+{
+    int n;
+
+    cout << "Enter n: ";
+    cin >> n;
+
+    if (n == 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return 1.0 / n + h_function(n - 1);
+    }
+}
